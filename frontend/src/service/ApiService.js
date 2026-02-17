26674f079537a2ba4d1188b084d963334b6a87db
Pavan kalyan Pachipala <150685987+Pa1-kalyan@users.noreply.github.com>
2026-02-17 17:15:18 +0530

    Update ApiService.js

--- frontend/src/service/ApiService.js
+++ frontend/src/service/ApiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class ApiService {
 
-    static BASE_URL = "http://localhost:2424";
+    static BASE_URL = "https://ecommerce-website-mkgy.onrender.com";
 
     static getHeader() {
         const token = localStorage.getItem("token");
